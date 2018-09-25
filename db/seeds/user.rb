bb0d7b0199976250bea86e4ff62c8134937e8052
shunta ichikawa <shunta27ichikawa@gmail.com>
2018-09-25 15:25:23 +0900

    seed微調整

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,6 +1,9 @@
 test1_user = User.new(id: 1, name: 'test1', email: 'test1@test.com', password: 'password', created_at: Time.current, updated_at: Time.current)
+test1_user.skip_confirmation!
 test1_user.save!
 test2_user = User.new(id: 2, name: 'test2', email: 'test2@test.com', password: 'password', created_at: Time.current, updated_at: Time.current)
+test2_user.skip_confirmation!
 test2_user.save!
 test3_user = User.new(id: 3, name: 'test3', email: 'test3@test.com', password: 'password', created_at: Time.current, updated_at: Time.current)
+test3_user.skip_confirmation!
 test3_user.save!
