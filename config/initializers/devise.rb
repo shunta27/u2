efa8c0a7156a817b4c29a02ecfb6d4ea80b174e6
shunta ichikawa <shunta27ichikawa@gmail.com>
2018-10-08 11:14:17 +0900

    サービス名をu2トークからメンタルルに変更

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '0a1ff1b3d884679e31dd14f32bc6aae695d0f7365c13cf41705b77e00b96e5a9b068c873dbaeb403140ff4d4a9dcf60ac53552aa91cd51696a650be2050cbc52'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'noreply@mentalulu.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
