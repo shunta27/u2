15d04088dfd2b07cad111c891c10cad6ae1f7e6c
shunta ichikawa <shunta27ichikawa@gmail.com>
2019-05-04 03:45:12 +0900

    tweak

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,63 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190216121636) do
+ActiveRecord::Schema.define(version: 2019_02_16_121636) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name",       limit: 255, default: "", null: false
-    t.string   "code",       limit: 255, default: "", null: false
-    t.integer  "sort_num",   limit: 4,   default: 0,  null: false
+  create_table "categories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "code", default: "", null: false
+    t.integer "sort_num", default: 0, null: false
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.integer  "user_id",     limit: 4
-    t.integer  "category_id", limit: 4
-    t.string   "subject",     limit: 255,   default: "", null: false
-    t.text     "body",        limit: 65535,              null: false
+  create_table "posts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "category_id"
+    t.string "subject", default: "", null: false
+    t.text "body", null: false
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
-
-  create_table "replies", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.integer  "post_id",    limit: 4
-    t.text     "body",       limit: 65535, null: false
+  create_table "replies", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "body", null: false
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["post_id"], name: "index_replies_on_post_id"
+    t.index ["user_id"], name: "index_replies_on_user_id"
   end
 
-  add_index "replies", ["post_id"], name: "index_replies_on_post_id", using: :btree
-  add_index "replies", ["user_id"], name: "index_replies_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name",                   limit: 190,   default: "", null: false
-    t.string   "profile_text",           limit: 1000,  default: ""
-    t.string   "email",                  limit: 190,   default: "", null: false
-    t.text     "avatar",                 limit: 65535
-    t.string   "encrypted_password",     limit: 255,   default: "", null: false
-    t.string   "reset_password_token",   limit: 190
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
+    t.string "name", limit: 190, default: "", null: false
+    t.string "profile_text", limit: 1000, default: ""
+    t.string "email", limit: 190, default: "", null: false
+    t.text "avatar"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token", limit: 190
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string   "confirmation_token",     limit: 190
+    t.string "confirmation_token", limit: 190
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
+    t.string "unconfirmed_email"
     t.datetime "deleted_at"
-    t.datetime "created_at",                                        null: false
-    t.datetime "updated_at",                                        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
