4e6a04b5cf153d941b6e3c68d50fd66a5b8cbdf3
shunta ichikawa <shunta27ichikawa@gmail.com>
2019-05-04 17:35:25 +0900

    tweak dot env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
+    :address => ENV['EMAIL_SMTP_ADDRESS'],
     :port => 587,
     :user_name => ENV['EMAIL_USERNAME'],
     :password => ENV['EMAIL_PASSWORD'],
